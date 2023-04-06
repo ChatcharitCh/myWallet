8fa61a02bdd08ce7f7185ecf7b6f97b34a618aac
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-04-06 16:09:50 +0700

    Design Model

--- walletApp/models.py
+++ walletApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Statement(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    choices = (("income", "income"), ("expense", "expense"))
+    category = models.CharField(max_length=100, choices=choices)
