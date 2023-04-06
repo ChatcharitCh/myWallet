b7f89cfd96b3ac069e4543f438262e64b87644b5
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-04-06 16:35:43 +0700

    Admin Panel

--- walletApp/models.py
+++ walletApp/models.py
@@ -5,4 +5,7 @@ class Statement(models.Model):
     name = models.CharField(max_length=100)
     amount = models.IntegerField()
     choices = (("income", "income"), ("expense", "expense"))
-    category = models.CharField(max_length=100, choices=choices)
+    category = models.CharField(max_length=100, choices=choices,default=1)
+
+    def __str__(self):
+        return self.name + "| " + str(self.amount) + "| " + self.category
