a2c7510b3f68947e03b74c631ac181e41bb9c271
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-04-08 11:06:11 +0700

    Admin : List_Editable

--- walletApp/admin.py
+++ walletApp/admin.py
@@ -4,6 +4,7 @@ from walletApp.models import Statement
 class StatementAdmin(admin.ModelAdmin):
     list_display = ["name", "amount", "category"]
     list_per_page = 10
+    list_editable = ["amount", "category"]
 
 # Register your models here.
 admin.site.register(Statement, StatementAdmin)
