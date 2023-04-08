7b804ff03c1bec20d1169203fc7d0e94701cd5b5
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-04-08 11:08:38 +0700

    Admin : List_Filter

--- walletApp/admin.py
+++ walletApp/admin.py
@@ -5,6 +5,7 @@ class StatementAdmin(admin.ModelAdmin):
     list_display = ["name", "amount", "category"]
     list_per_page = 10
     list_editable = ["amount", "category"]
+    list_filter = ["category", "amount"]
 
 # Register your models here.
 admin.site.register(Statement, StatementAdmin)
