6d5c9232fa07b822fe91798e415c7d613321f051
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-04-08 11:00:41 +0700

    Admin : List_Display

--- walletApp/admin.py
+++ walletApp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from walletApp.models import Statement
 
+class StatementAdmin(admin.ModelAdmin):
+    list_display = ["name", "amount", "category"]
+
 # Register your models here.
-admin.site.register(Statement)
+admin.site.register(Statement, StatementAdmin)
